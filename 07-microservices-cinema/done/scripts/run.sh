c8af15981c6402e0d1f272915284184d1a1db804
Jeroenvdl <jeroenvdl@outlook.com>
2019-02-28 21:09:03 +0100

    name change for db to cinema-mongo

--- 07-microservices-cinema/done/scripts/run.sh
+++ 07-microservices-cinema/done/scripts/run.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR}
 
 docker run --name cinema-traefik -d -p 8080:8080 -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock cinema-traefik
-docker run --name db -d db 
+docker run --name db -d cinema-mongo
 docker exec db /backup/restore.sh
 docker run --name cinema-bookings --link db:db -d cinema-bookings
 docker run --name cinema-showtimes --link db:db -d cinema-showtimes
